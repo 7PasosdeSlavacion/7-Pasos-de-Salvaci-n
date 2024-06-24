c179eb6c37a0abe717a980948a5499edfe4b7c99
7PasosdeSlavacion <162037414+7PasosdeSlavacion@users.noreply.github.com>
2024-06-23 19:33:04 -0500

    js.js

--- js/js.js
+++ js/js.js
@@ -33,6 +33,6 @@ btnCuenta.addEventListener('click', () => {
 
 document.addEventListener("DOMContentLoaded", function() {
     var base_url = document.createElement('base');
-    base_url.href = 'https://7pasosdeslavacion.github.io/';
+    base_url.href = 'https://7pasosdeslavacion.github.io/7-Pasos-de-Salvaci-n/';
     document.head.appendChild(base_url);
 });
