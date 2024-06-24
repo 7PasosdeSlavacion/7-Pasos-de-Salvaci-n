8f5dac95b5dcbe70b6d475e6f64ec518fb921b97
7PasosdeSlavacion <162037414+7PasosdeSlavacion@users.noreply.github.com>
2024-06-24 05:40:47 -0500

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var base_url = document.createElement('base');
+    base_url.href = 'https://7pasosdeslavacion.github.io/7-Pasos-de-Salvaci-n/';
+    document.head.appendChild(base_url);
+});
